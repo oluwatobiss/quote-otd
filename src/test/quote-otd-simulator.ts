d2b7a4aa05917888ddc27dbf1f94a60315e0c104
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2026-07-25 02:36:38 +0100

    chore: Create testbed for exercising the quote-otd smart contract in tests

---
+++ src/test/quote-otd-simulator.ts
@@ -0,0 +1,69 @@
+import {
+  type CircuitContext,
+  QueryContext,
+  sampleContractAddress,
+  createConstructorContext,
+  CostModel,
+} from "@midnight-ntwrk/compact-runtime";
+import {
+  Contract,
+  type Ledger,
+  ledger,
+} from "../../contracts/managed/quote-otd/contract/index.js";
+import { type QuoteOTDPrivateState, witnesses } from "../witnesses.js";
+
+export class QuoteOTDSimulator {
+  readonly contract: Contract<QuoteOTDPrivateState>;
+  circuitContext: CircuitContext<QuoteOTDPrivateState>;
+
+  constructor(secretKey: Uint8Array) {
+    this.contract = new Contract<QuoteOTDPrivateState>(witnesses);
+    const {
+      currentPrivateState,
+      currentContractState,
+      currentZswapLocalState,
+    } = this.contract.initialState(
+      createConstructorContext({ secretKey }, "0".repeat(64)),
+    );
+    this.circuitContext = {
+      currentPrivateState,
+      currentZswapLocalState,
+      costModel: CostModel.initialCostModel(),
+      currentQueryContext: new QueryContext(
+        currentContractState.data,
+        sampleContractAddress(),
+      ),
+    };
+  }
+
+  // Switch to a different secret key for a different user
+  public switchUser(secretKey: Uint8Array) {
+    this.circuitContext.currentPrivateState = {
+      secretKey,
+    };
+  }
+
+  public getLedger(): Ledger {
+    return ledger(this.circuitContext.currentQueryContext.state);
+  }
+
+  public getPrivateState(): QuoteOTDPrivateState {
+    return this.circuitContext.currentPrivateState;
+  }
+
+  public post(newQuote: string): Ledger {
+    // Update the current context to be the result of executing the circuit.
+    this.circuitContext = this.contract.impureCircuits.post(
+      this.circuitContext,
+      newQuote,
+    ).context;
+    return ledger(this.circuitContext.currentQueryContext.state);
+  }
+
+  public publicKey(): Uint8Array {
+    return this.contract.circuits.publicKey(
+      this.circuitContext,
+      this.getPrivateState().secretKey,
+    ).result;
+  }
+}
